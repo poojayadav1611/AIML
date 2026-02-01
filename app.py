8bca24efbedd28482c91dc60ca7a978b962cbfea
Pooja Yadav <poojayadav@172-15-0-144.lightspeed.stlsmo.sbcglobal.net>
2026-02-01 23:45:02 +0530

    update streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import streamlit as st
+
+st.title("My Streamlit App")
+st.write("Hello from Render deployment 🚀")
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
